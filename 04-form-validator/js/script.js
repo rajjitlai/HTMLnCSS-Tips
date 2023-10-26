e947ba289c5bb5b1ed5ae63e53f03bb92e7c29dc
RAJJIT <2.0lairaj@gmail.com>
2023-10-26 14:22:16 +0530

    changes-ev

--- 04-form-validator/js/script.js
+++ 04-form-validator/js/script.js
@@ -4,7 +4,3 @@ const iti = window.intlTelInput(input, {
     utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.0/js/utils.js",
 });
 
-input.addEventListener("input", function () {
-    const phoneNumber = iti.getNumber();
-    // console.log(phoneNumber);
-});
